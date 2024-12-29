90cd6b15baca2b80453e9ed861ba055262f77bb2
Alexandre Silva Santos <57051760+alexff2@users.noreply.github.com>
2024-12-29 15:43:14 +0000

    Versao para namorada

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,27 @@
 function Home() {
-    return <h1>Bobo, vc é a melhor pessoa do mundo, te amo maior que o universo!</h1>
+    return (
+        <div
+            style={{
+                background: 'url(https://static.vecteezy.com/ti/fotos-gratis/p1/35987260-ai-gerado-dia-dos-namorados-dia-vermelho-coracoes-do-amor-com-bling-fundos-gratis-foto.jpg)',
+                position: 'absolute',
+                height: '100%',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center'
+            }}
+        >
+            <h1
+                style={{
+                  backgroundColor: 'white',
+                  padding: '50px',
+                  margin: '50px',
+                  borderRadius: '20px',
+                  opacity: '0.6',
+                  fontFamily: 'sans-serif'
+                }}
+            >Bobo, vc é a melhor pessoa do mundo, te amo maior que o universo!</h1>
+        </div>
+    )
 }
 
 export default Home
